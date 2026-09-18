4ccc1440f21ef666912cd4f677d478ca899541b0
id3082 <adam.benamer@eleve.ensai.fr>
2026-09-18 07:50:29 +0000

    yhf

--- Classes/Classes_Service/ReservationsService.py
+++ Classes/Classes_Service/ReservationsService.py
@@ -4,5 +4,12 @@ from Classes.ClasseService import ClientService
 class ReservationService:
     
     def __init__(self):
-        self.
+        pass
+    def creer(id_client: int, place: str, classe:int):
+        pass
+
+    def annuler(id_reservation: int):
+        pass
+    def historique_par_client(id_client: int):
+        pass
         
