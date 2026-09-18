156eabf3de621231f2ce5d26af0e32803af3bfd0
id2948 <nathan.tiatia@eleve.ensai.fr>
2026-09-18 07:23:26 +0000

    commit très important

--- Classes/Classes_Objets/Utilisateurs/Utilisateur.py
+++ Classes/Classes_Objets/Utilisateurs/Utilisateur.py
@@ -1,4 +1,17 @@
 class Utilisateur:
+    """Modélise une personne utilisant l'application.
+
+    Param:
+    ------
+    id: int
+        identifiant de l'utilisateur
+    mdp: str
+        mot de passe de l'utilisateur
+    nom_utilisateur: str
+        nom de l'utilisateur
+    role: str
+        niveau d'habilitation de l'utilisateur
+    """
 
     def __init__(self, id: int, mdp: str, nom_utilisateur: str, role: str):
         assert len(nom_utilisateur) < 16, "Le nom doit faire 15 caractères maximum"
