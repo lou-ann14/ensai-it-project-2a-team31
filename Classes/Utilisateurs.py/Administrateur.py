35cf7d91352efd6fe12b60ad9491753b9cbe754d
id3082 <adam.benamer@eleve.ensai.fr>
2026-09-18 06:37:18 +0000

    Administrateur

--- Classes/Utilisateurs.py/Administrateur.py
+++ Classes/Utilisateurs.py/Administrateur.py
@@ -2,4 +2,7 @@ from classes.utilisateurs import Utilisateur
 
 
 class Administrateur(Utilisateur):
-    def __init__(self,):
+    """Classe administrateur, heritée de la classe Utilisateur qui modélise les fonctionnalités de l'administrateur"""
+    
+
+
