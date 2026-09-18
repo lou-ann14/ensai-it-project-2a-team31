28bda53f6a74d3e1b260a4f36f7f3facd7c2de69
lou-ann14 <lou-ann.clement@eleve.ensai.fr>
2026-09-18 06:41:22 +0000

    mods pour ruff

--- Classes/Utilisateurs/Administrateur.py
+++ Classes/Utilisateurs/Administrateur.py
@@ -2,7 +2,5 @@ from classes.utilisateurs import Utilisateur
 
 
 class Administrateur(Utilisateur):
-    """Classe administrateur, heritée de la classe Utilisateur qui modélise les fonctionnalités de l'administrateur"""
-    
-
-
+    """Classe administrateur, heritée de la classe Utilisateur qui modélise les fonctionnalités de l
+    'administrateur"""
