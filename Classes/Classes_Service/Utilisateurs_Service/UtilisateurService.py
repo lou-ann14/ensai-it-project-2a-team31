c22598360bf424e2f1ec1e92ced2f7497ee5ed59
lou-ann14 <lou-ann.clement@eleve.ensai.fr>
2026-09-25 08:48:14 +0000

    modifs syntaxe 2

--- Classes/Classes_Service/Utilisateurs_Service/UtilisateurService.py
+++ Classes/Classes_Service/Utilisateurs_Service/UtilisateurService.py
@@ -1,5 +1,8 @@
+from ClassesDAO import UtilisateursDAO
+
+
 class UtilisateurService:
     """classe qui gère les actions des utilisateurs
     """
-    def __init__(self, utilisateur_dao: UtilisateurDAO):
+    def __init__(self, utilisateur_dao: UtilisateursDAO):
         self.utilisateur_dao = utilisateur_dao
