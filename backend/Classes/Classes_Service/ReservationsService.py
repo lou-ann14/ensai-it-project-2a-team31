65706b1557b469f835ca8976911070ef21614274
id3082 <adam.benamer@eleve.ensai.fr>
2026-09-25 12:37:24 +0000

    gh

--- backend/Classes/Classes_Service/ReservationsService.py
+++ backend/Classes/Classes_Service/ReservationsService.py
@@ -19,7 +19,34 @@ class ReservationService:
         pass
 
     def annuler(id_reservation: int):
+
+        """
+        Méthode servant à annuler une réservation existante.
+
+        Param:
+        ------
+        id_reservation: int
+            l'identifiant unique de la réservation à annuler
+
+        Return:
+        ------
+        bool
+            True si l'annulation a réussi, False sinon
+        """
+        
         pass
 
     def historique_par_client(id_client: int):
+        """
+         Methode servant à créer une réservation.
+
+        Param:
+        ------
+        id_client: int
+            l'id du client
+        place: str
+            le numéro de place de la reservation
+        classe: int
+             la classe choisie lors de la réservation 
+        """
         pass
