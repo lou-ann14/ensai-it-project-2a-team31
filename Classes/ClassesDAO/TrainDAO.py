2f3ca1460dbb119423121cd36456fd9eb9f594bd
lou-ann14 <lou-ann.clement@eleve.ensai.fr>
2026-09-25 12:19:42 +0000

    modifs syntaxe 4

--- Classes/ClassesDAO/TrainDAO.py
+++ Classes/ClassesDAO/TrainDAO.py
@@ -6,13 +6,8 @@ class TrainDAO:
     def creer(self, id_train: int, type_train: str, places_initiales: str) -> Train:
         return Train(id_train, type_train, places_initiales)
 
-    def modifier_capacite(self, id_train: int, capacite: int) -> Train : 
+    def modifier_capacite(self, id_train: int, capacite: int) -> Train:
         pass
 
     def trouver_par_id(self, id_train: int) -> Train:
         pass
-
-
-
-
-
