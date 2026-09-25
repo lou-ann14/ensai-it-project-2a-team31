8bb40c25397de94c4f85a1126f711edcbddc879f
laurine11 <laurine.leroy@eleve.ensai.fr>
2026-09-25 12:23:58 +0000

    ruff

---
+++ Classes/ClassesDAO/GareDAO.py
@@ -0,0 +1,9 @@
+from Classes_Objets.Gare import Gare
+
+
+class GareDAO:
+    def rechercher(self, nom: str | None, ville: str | None) -> Gare:
+        pass
+
+    def trouver_par_id(self, id_gare: int) -> Gare:
+        pass
