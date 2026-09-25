79e9a828ca9aeebfdedcff8742b02001de1accc2
id3082 <adam.benamer@eleve.ensai.fr>
2026-09-25 10:03:43 +0000

    LigneDAO

---
+++ Classes/ClassesDAO/LigneDAO.py
@@ -0,0 +1,12 @@
+
+
+
+class LigneDAO:
+    def creer(ligne: Ligne) -> Ligne:
+        pass
+
+    def rechercher(gare_depart: Gare, gare_arrivee: Gare) -> Gare:
+        pass
+
+    def trouver_par_id(id_ligne: int) -> Ligne:
+        pass
